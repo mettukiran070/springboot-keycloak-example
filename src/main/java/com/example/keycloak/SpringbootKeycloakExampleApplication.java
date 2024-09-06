d517f8f2ae2faa40587e607ea54adaaa2253ca9a
Mettu Kiran Kumar Reddy <mettu.kiran070@gmail.com>
2024-09-06 21:42:18 +0530

    initial commit

---
+++ src/main/java/com/example/keycloak/SpringbootKeycloakExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKeycloakExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKeycloakExampleApplication.class, args);
+	}
+
+}
