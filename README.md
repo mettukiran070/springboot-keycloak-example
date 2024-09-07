27ef058cbd8f71c4b01dfcf36c297120beb666b4
Mettu Kiran Kumar Reddy <mettu.kiran070@gmail.com>
2024-09-07 19:52:58 +0530

    Updated the project

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # springboot-keycloak-example
 
+https://medium.com/javarevisited/keycloak-integration-with-spring-security-6-37999f43ec85
+
 https://medium.com/@disa2aka/docker-deployments-for-keycloak-and-postgresql-e75707b155e5
---
+++ src/main/java/com/example/keycloak/security/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.example.keycloak.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  public static final String ADMIN = "admin";
+  public static final String USER = "user";
+  private final JwtConverter jwtConverter;
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests((auth) ->
+//        auth.requestMatchers(HttpMethod.GET, "/api/hello").permitAll()
+//            .requestMatchers(HttpMethod.GET, "/api/admin/**").hasRole(ADMIN)
+//            .requestMatchers(HttpMethod.GET, "/api/user/**").hasRole(USER)
+            auth.requestMatchers("/products/**").hasAnyRole(ADMIN,USER)
+                .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/swagger-resources/**", "/webjars/**").permitAll()
+            .anyRequest().authenticated());
+
+    http.sessionManagement(sess -> sess.sessionCreationPolicy(
+        SessionCreationPolicy.STATELESS));
+    http.oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtConverter)));
+
+    return http.build();
+  }
+
+}
